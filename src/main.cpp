6ddb6da73d5c1c55880675dc537ddb02213b10c9
shadow-lr <shaodow_lr@163.com>
2022-04-09 02:42:32 +0800

    Instance Rotation

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include "geometry/ray.h"
 #include "math/vec3.h"
 #include "rtweekend.h"
-#include "shape/moving_sphere.h"
 #include "color.h"
+
 #include "geometry/hittable_list.h"
+#include "geometry/translate.h"
+#include "geometry/rotate.h"
+
 #include "shape/sphere.h"
 #include "shape/aarect.h"
 #include "shape/box.h"
+#include "shape/moving_sphere.h"
 
 #include "asset/material.h"
 #include "asset/camera.h"
@@ -177,8 +181,15 @@ hittable_list cornell_box() {
     auto light = make_shared<diffuse_light>(color(15, 15, 15));
 
     // box
-    objects.add(make_shared<box>(point3(130, 0, 65), point3(295, 165, 230), white));
-    objects.add(make_shared<box>(point3(265, 0, 295), point3(430, 330, 460), white));
+    shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    objects.add(box1);
+
+    shared_ptr<hittable> box2 = make_shared<box>(point3(0,0,0), point3(165,165,165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+    objects.add(box2);
 
     // wall
     objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
