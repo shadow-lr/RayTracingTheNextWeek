01b1176397be189140f81edeaf67f0be9b93bab3
shadow-lr <shaodow_lr@163.com>
2022-04-08 21:28:45 +0800

    More Axis-Aligned Rectangles

--- include/shape/aarect.h
+++ include/shape/aarect.h
@@ -10,29 +10,7 @@ public:
     xy_rect(double x0_, double x1_, double y0_, double y1_, double k_, shared_ptr<material> mat)
             : x0(x0_), x1(x1_), y0(y0_), y1(y1_), k(k_), mp(mat) {}
 
-    virtual bool hit(const ray &r, double t_min, double t_max, hit_record &rec) const override{
-        auto t = (k - r.orig.z()) / r.direction().z();
-
-        if (t < t_min || t > t_max)
-            return false;
-
-        // hit point
-        double hit_x = r.origin().x() + t * r.direction().x();
-        double hit_y = r.origin().y() + t * r.direction().y();
-
-        if (hit_x < x0 || hit_x > x1 || hit_y < y0 || hit_y > y1)
-            return false;
-
-        rec.u = (hit_x - x0) / (x1 - x0);
-        rec.v = (hit_y - y0) / (y1 - y0);
-        rec.t = t;
-        // todo: may cause some error(z )
-        auto outward_normal = vec3(0, 0, 1);
-        rec.set_face_normal(r, outward_normal);
-        rec.mat_ptr = mp;
-        rec.p = r.at(t);
-        return true;
-    }
+    virtual bool hit(const ray &r, double t_min, double t_max, hit_record &rec) const override;
 
     virtual bool bounding_box(double time0, double time1, aabb &output_box) const override {
         // The bounding box must have non-zero width in each dimension, so pad the Z
@@ -46,4 +24,48 @@ public:
     shared_ptr<material> mp;
     // z = k
     double x0{}, x1{}, y0{}, y1{}, k{};
+};
+
+class xz_rect : public hittable {
+public:
+    xz_rect() {}
+
+    xz_rect(double _x0, double _x1, double _z0, double _z1, double _k,
+            shared_ptr<material> mat)
+            : x0(_x0), x1(_x1), z0(_z0), z1(_z1), k(_k), mp(mat) {};
+
+    virtual bool hit(const ray &r, double t_min, double t_max, hit_record &rec) const override;
+
+    virtual bool bounding_box(double time0, double time1, aabb &output_box) const override {
+        // The bounding box must have non-zero width in each dimension, so pad the Y
+        // dimension a small amount.
+        output_box = aabb(point3(x0, k - 0.0001, z0), point3(x1, k + 0.0001, z1));
+        return true;
+    }
+
+public:
+    shared_ptr<material> mp;
+    double x0, x1, z0, z1, k;
+};
+
+class yz_rect : public hittable {
+public:
+    yz_rect() {}
+
+    yz_rect(double _y0, double _y1, double _z0, double _z1, double _k,
+            shared_ptr<material> mat)
+            : y0(_y0), y1(_y1), z0(_z0), z1(_z1), k(_k), mp(mat) {};
+
+    virtual bool hit(const ray &r, double t_min, double t_max, hit_record &rec) const override;
+
+    virtual bool bounding_box(double time0, double time1, aabb &output_box) const override {
+        // The bounding box must have non-zero width in each dimension, so pad the X
+        // dimension a small amount.    
+        output_box = aabb(point3(k - 0.0001, y0, z0), point3(k + 0.0001, y1, z1));
+        return true;
+    }
+
+public:
+    shared_ptr<material> mp;
+    double y0, y1, z0, z1, k;
 };
